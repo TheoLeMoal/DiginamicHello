5aef4a593d770d31d99b8f0334e2d2f867fe4235
Theo <contact@theo-le-moal.fr>
2024-04-09 15:15:31 +0200

    TP 4 terminé

--- src/main/java/com/diginamic/Diginamic/controleurs/VilleControleur.java
+++ src/main/java/com/diginamic/Diginamic/controleurs/VilleControleur.java
@@ -1,11 +1,17 @@
 package com.diginamic.Diginamic.controleurs;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import com.diginamic.Diginamic.model.Ville;
 
@@ -13,14 +19,38 @@ import com.diginamic.Diginamic.model.Ville;
 @RequestMapping("/villes")
 public class VilleControleur {
 	
-	@GetMapping
-	public List<Ville> villes() {
-        return Arrays.asList(
-                new Ville("Paris", 2175601),
-                new Ville("Marseille", 855393),
-                new Ville("Lyon", 513275),
-                new Ville("Toulouse", 479553),
-                new Ville("Nice", 342295)
-            );
-	}
+    private static List<Ville> listeVilles = new ArrayList<Ville>();
+    
+    static {
+    	listeVilles.add(new Ville("Paris", 2175601));
+    	listeVilles.add(new Ville("Marseille", 855393));
+    	listeVilles.add(new Ville("Lyon", 513275));
+    	listeVilles.add(new Ville("Toulouse", 479553));
+    	listeVilles.add(new Ville("Nice", 342295));
+    }
+
+	
+    @GetMapping
+    public List<Ville> getListeVilles() {
+        return listeVilles;
+    }
+	
+    @PostMapping
+    public ResponseEntity<String> ajouterVille(@RequestBody Ville nouvelleVille) {
+        if (villeExistante(nouvelleVille)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("La ville existe déjà");
+        }
+        
+        listeVilles.add(nouvelleVille);
+        return ResponseEntity.status(HttpStatus.OK).body("Ville insérée avec succès");
+    }
+
+    private boolean villeExistante(Ville nouvelleVille) {
+        for (Ville ville : listeVilles) {
+            if (ville.getNom().equalsIgnoreCase(nouvelleVille.getNom())) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
